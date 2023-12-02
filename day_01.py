c4029a08bb17fdb10fc2e61697d8e3020b6d9395
rubennoriegamier <rubennoriegamier@gmail.com>
2023-12-02 12:34:27 +0100

    Refactor day 1 regex

--- day_01.py
+++ day_01.py
@@ -18,14 +18,14 @@ def part_1(cal_vals: list[str]) -> int:
 def part_2(cal_vals: list[str]) -> int:
     ltrs = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
     dgt_re = re.compile(fr'\d|{'|'.join(ltrs)}')
-    rev_dgt_re = re.compile(fr'\d|{'|'.join(ltrs)[::-1]}')
+    last_dgt_re = re.compile(f'.*({dgt_re.pattern})')
     to_dgt = {ltr: str(dgt) for dgt, ltr in enumerate(ltrs, 1)}
 
     sum_ = 0
 
     for cal_val in cal_vals:
         first_dgt = dgt_re.search(cal_val).group()
-        last_dgt = rev_dgt_re.search(cal_val[::-1]).group()[::-1]
+        last_dgt = last_dgt_re.search(cal_val).group(1)
 
         sum_ += int(to_dgt.get(first_dgt, first_dgt) +
                     to_dgt.get(last_dgt, last_dgt))
