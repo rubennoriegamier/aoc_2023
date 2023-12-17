ba045933e57a63bddb6ff73848e9b9044f1b7073
rnoriega <rnoriega@flameanalytics.com>
2023-12-17 19:48:00 +0100

    Day 17, part 2: optimize

--- day_17.py
+++ day_17.py
@@ -20,18 +20,19 @@ def create_graph(heat_map: list[list[int]], *, min_dist: int, max_dist: int) -> 
 
     for y in range(h):
         for x in range(w):
-            for distance in range(1, min(x + 1, max_dist + 1)):
-                vs.append((y, x, '>', distance))
-            for distance in range(1, min(y + 1, max_dist + 1)):
-                vs.append((y, x, 'v', distance))
-            for distance in range(1, min(w - x, max_dist + 1)):
-                vs.append((y, x, '<', distance))
-            for distance in range(1, min(h - y, max_dist + 1)):
-                vs.append((y, x, '^', distance))
+            for dist in range(1, min(x + 1, max_dist + 1)):
+                vs.append((y, x, '>', dist))
+            for dist in range(1, min(y + 1, max_dist + 1)):
+                vs.append((y, x, 'v', dist))
+            for dist in range(1, min(w - x, max_dist + 1)):
+                vs.append((y, x, '<', dist))
+            for dist in range(1, min(h - y, max_dist + 1)):
+                vs.append((y, x, '^', dist))
 
     g = Graph(directed=True)
     g.add_vertices(vs)
-    get_v = {v['name']: v for v in g.vs}
+    get_v = {v: i for i, v in enumerate(vs)}
+
     tl = get_v[vs[0]]
     br = get_v[vs[1]]
     es = [(tl, get_v[0, 1, '>', 1]),
@@ -44,9 +45,7 @@ def create_graph(heat_map: list[list[int]], *, min_dist: int, max_dist: int) -> 
         es.append((get_v[br_y, br_x, 'v', dist], br))
         ws.append(0)
 
-    for v in g.vs:
-        y, x, dir_, dist = v['name']
-
+    for (y, x, dir_, dist), v in get_v.items():
         match dir_:
             case '>':
                 if dist >= min_dist:
